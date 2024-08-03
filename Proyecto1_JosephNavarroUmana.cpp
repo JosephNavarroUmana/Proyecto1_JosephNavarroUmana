8e5265d369c131bd42dcabb012da12c311f8654f
Joseph <Joseph@DESKTOP-5GAS2R9>
2024-08-03 12:05:43 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto1_JosephNavarroUmana.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void menu() {
+
+	int menu;
+	cout << "\t\tMenu" << endl;
+	cout << "Archivo" << endl;
+	cout << "Mantenimiento" << endl;
+	cout << "Reserva" << endl;
+	cout << "Venta" << endl;
+	cin >> menu;
+
+	switch (menu)
+	{
+	case 1:
+		//archivo
+		cout << "Acerca de " << endl;
+		cout << "Salir" << endl;
+		cin >> menu;
+		switch (menu)
+		{
+		case 1:
+			cout << "Hola";
+			break;
+		case 2:
+			cout << "Adios";
+		default:
+			break;
+		}
+		break;
+	case 2:
+		//mantenimiento
+		break;
+	case 3:
+		//reservar
+		break;
+	case 4:
+		//venta
+		break;
+	default:
+		break;
+	}
+}
+
+int main()
+{
+	menu();
+}
+
